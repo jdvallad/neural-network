eae555128a2c9ab5523ebe34c74e939449c1d24f
Joshua Valladares <joshuadvalladares@gmail.com>
2021-09-15 19:53:46 -0700

    script commit

--- Driver.java
+++ Driver.java
@@ -3,15 +3,15 @@ import java.util.Map;
 public class Driver {
     public static void main(String[] args) throws Exception {
         DataIterator nums = new mnistIterator(100, 420);
-        nn model = new nn(28 * 28, "logLoss", "./Data/mnistDigitCategorize.ser");
+        nn model = new nn(28 * 28, "logLoss", "./serials/mnistDigitCategorize.ser");
         model.add("sigmoid", 16);
         model.add("leakyRelu", 16);
         model.add("sigmoid", 10);
-        //   model.build();
-        model = nn.load("./Data/mnistDigitCategorize.ser");
+        model.build();
+       // model = nn.load("./serials/mnistDigitCategorize.ser");
         Map<String, Object> input = Map.of("width", 28, "height", 28, "color", false, "scale", 10.);
         Map<String, Object> output = Map.of("width", 10, "height", 1, "color", false, "scale", 80.);
-        model.train(nums, 10, .5, false);
+        model.train(nums, 10, .5, true, input, output);
         model.getAccuracy(nums);
     }
 }
